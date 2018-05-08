ac749c0e792a7036030e06b43c6fefc58387c71f
Brian Henry and Noah Medoff <noah@>
2018-05-08 09:44:01 -0700

    initial commit. create university database with ability to add students test passing

---
+++ UniversityRegistrar.Tests/ModelTests/UniversityTests.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using UniversityRegistrar.Models;
+using UniversityRegistrar;
+using MySql.Data.MySqlClient;
+
+namespace UniversityRegistrar.Tests
+{
+  [TestClass]
+  public class UniversityTest : IDisposable
+  {
+    public UniversityTest()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=university_test;";
+    }
+    public void Dispose()
+    {
+      Student.DeleteAll();
+    }
+    [TestMethod]
+    public void Save_StudentSavesToDatabase_Students()
+    {
+      //Arrange
+      Student testStudent = new Student("Tim", "08/11/21");
+      testStudent.Save();
+
+      //Act
+      List<Student> studentResult = Student.GetAll();
+      List<Student> studentList = new List<Student>{testStudent};
+
+      //Assert
+      CollectionAssert.AreEqual(studentList, studentResult);
+    }
+
+  }
+}
---
+++ UniversityRegistrar/Models/Student.cs
@@ -0,0 +1,120 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System;
+
+namespace UniversityRegistrar.Models
+{
+  public class Student
+  {
+    private int _student_id;
+    private string _name;
+    private string _date;
+
+    public Student(string name, string date, int student_id = 0)
+    {
+      _student_id = student_id;
+      _name = name;
+      _date = date;
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public int GetId()
+    {
+        return _student_id;
+    }
+    public string GetDate()
+    {
+        return _date;
+    }
+    public void Save()
+    {
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+
+        var cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = @"INSERT INTO students (name, date) VALUES (@name, @date);";
+
+        MySqlParameter name = new MySqlParameter();
+        name.ParameterName = "@name";
+        name.Value = this._name;
+        cmd.Parameters.Add(name);
+
+        MySqlParameter date= new MySqlParameter();
+        date.ParameterName = "@date";
+        date.Value = this._date;
+        cmd.Parameters.Add(date);
+
+        // Code to declare, set, and add values to a categoryId SQL parameters has also been removed.
+
+        cmd.ExecuteNonQuery();
+        _student_id = (int) cmd.LastInsertedId;
+        conn.Close();
+        if (conn != null)
+        {
+            conn.Dispose();
+        }
+    }
+    public static List<Student> GetAll()
+    {
+        List<Student> allStudents = new List<Student> {};
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+        var cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = @"SELECT * FROM students;";
+        var rdr = cmd.ExecuteReader() as MySqlDataReader;
+        while(rdr.Read())
+        {
+          int studentId = rdr.GetInt32(0);
+          string studentName = rdr.GetString(1);
+          string studentDate = rdr.GetString(2);
+          // We no longer need to read categoryIds from our items table here.
+          // Constructor below no longer includes a itemCategoryId parameter:
+          Student newStudent = new Student(studentName, studentDate, studentId);
+          allStudents.Add(newStudent);
+        }
+        conn.Close();
+        if (conn != null)
+        {
+            conn.Dispose();
+        }
+        return allStudents;
+    }
+    public static void DeleteAll()
+    {
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+        var cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = @"DELETE FROM students;";
+        cmd.ExecuteNonQuery();
+        conn.Close();
+        if (conn != null)
+        {
+            conn.Dispose();
+        }
+    }
+    public override bool Equals(System.Object otherStudent)
+    {
+      if (!(otherStudent is Student))
+      {
+        return false;
+      }
+      else
+      {
+         Student newStudent = (Student) otherStudent;
+         bool idEquality = this.GetId() == newStudent.GetId();
+         bool nameEquality = this.GetName() == newStudent.GetName();
+         // We no longer compare Students' categoryIds in a categoryEquality bool here.
+         return (idEquality && nameEquality);
+       }
+    }
+    public override int GetHashCode()
+    {
+         return this.GetName().GetHashCode();
+    }
+
+  }
+}
