7b0afbae9c8fafebc23c746017cf1e5285879e73
Brian Henry and Noah Medoff <brian@>
2018-05-08 15:36:01 -0700

    start on front end create index

---
+++ UniversityRegistrar/Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using UniversityRegistrar.Models;
+
+namespace UniversityRegistrar.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            List<Student> allStudents = Student.GetAll();
+            return View(allStudents);
+        }
+    }
+}
